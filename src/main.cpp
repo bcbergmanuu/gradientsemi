139ba74f3e19b7ab240863b5f48bdb9bd50baa0c
boris <b.c.bergman@uu.nl>
2023-06-12 18:26:53 +0200

    delay 100ms

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <Arduino.h>
 
+enum measure_state 
+{
+  st_measure,
+  st_switch,
+};
+
+enum switchvalue 
+{
+  sw_OFF  = 0,
+  sw_LED0 = 6,
+  sw_LED1 = 7,
+  sw_LED2 = 8,
+  sw_LED3 = 9,
+};
+
 // put function declarations here:
 int screenvalue = 0;
 
 void setup() {
-  //Serial.begin(115200);
+  Serial.begin(115200);
   pinMode(A0, INPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
@@ -15,44 +30,58 @@ void setup() {
   
 }
 
+measure_state currentstate = st_measure;
+
+switchvalue previousvalue = sw_OFF; //LED
+switchvalue currentvalue = sw_OFF;
+
+void setLED(switchvalue lednum) {  
+  uint8_t led_number = static_cast<uint8_t>(lednum);
+  Serial.println("numbers");
+  for (uint8_t x = 6; x < 10; x++ ) {      
+      digitalWrite(x, (led_number == x) ? HIGH : LOW);      
+      Serial.print(x);
+      Serial.print("-");
+      Serial.print(led_number);
+      Serial.print("-");
+      Serial.print((led_number == x));
+      Serial.println("-");
+  }    
+
+}
+
+switchvalue getvalue() {
+    screenvalue = analogRead(A0);
+    if(screenvalue < 75) return sw_OFF;
+    
+    else if (screenvalue > 74 && screenvalue < 275) return sw_LED0;
+    else if (screenvalue > 274 && screenvalue < 475) return sw_LED1;
+    else if (screenvalue > 474 && screenvalue < 675) return sw_LED2;
+    return sw_LED3;
+}
+
 void loop() {
-  screenvalue = analogRead(A0);
- // Serial.println(screenvalue);
   
-    if(screenvalue < 75) {
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-      digitalWrite(8, LOW);
-      digitalWrite(9, LOW);
-    } 
-    else if (screenvalue > 74 && screenvalue < 275)
-    {      
-      digitalWrite(6, HIGH);
-      digitalWrite(7, LOW);
-      digitalWrite(8, LOW);
-      digitalWrite(9, LOW);
-    } 
-    else if (screenvalue > 274 && screenvalue < 475)
-    {        
-      digitalWrite(6, LOW);
-      digitalWrite(7, HIGH);
-      digitalWrite(8, LOW);
-      digitalWrite(9, LOW);
-    } 
-    else if (screenvalue > 474 && screenvalue < 675)
-    {
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-      digitalWrite(8, HIGH);
-      digitalWrite(9, LOW);
-    }
-    else if (screenvalue > 674)
-    {
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-      digitalWrite(8, LOW);
-      digitalWrite(9, HIGH);      
-    }
-  //delay(1000);
+  switch (currentstate)
+  {
+    case st_measure:
+      delay(100);
+      currentvalue = getvalue();
+      Serial.println(currentvalue);
+
+      if(currentvalue == previousvalue) {
+        currentstate = st_switch;
+      }      
+      previousvalue = currentvalue;      
+      break;
+    
+    case st_switch:
+      setLED(currentvalue);
+      currentstate = st_measure;
+      break;
+  }
+ 
+   
+  
 }
 
